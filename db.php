58cb4a43af73c38c54b1a753b53f1c944d4b62d7
valderramae08-hub <valderramae08@gmail.com>
2026-05-06 19:35:46 -0500

    corrigiendo estructura proyecto

--- db.php
+++ db.php
@@ -1,12 +1,22 @@
-<?php
-$host = "mysql-aldrinn.alwaysdata.net";
-$user = "TU_USUARIO";
-$pass = "TU_PASSWORD";
-$db   = "TU_BASE_DE_DATOS";
+name: Deploy to Alwaysdata
 
-$conn = new mysqli($host, $user, $pass, $db);
+on:
+  push:
+    branches:
+      - main
 
-if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
-}
-?>
+jobs:
+  deploy:
+    runs-on: ubuntu-latest
+
+    steps:
+      - name: Checkout code
+        uses: actions/checkout@v4
+
+      - name: Deploy via FTP
+        uses: SamKirkland/FTP-Deploy-Action@v4.3.5
+        with:
+          server: ftp.alwaysdata.com
+          username: TU_USUARIO
+          password: TU_PASSWORD
+          server-dir: www/cliente1/
